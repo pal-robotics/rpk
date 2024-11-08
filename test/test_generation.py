141c3d0b732fd8492f8b7fdcdf33fd99cc27553e
Séverin Lemaignan <severin.lemaignan@pal-robotics.com>
2024-11-08 11:45:11 +0100

    [test] test that all the templates compile

--- test/test_generation.py
+++ test/test_generation.py
@@ -12,12 +12,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from pathlib import Path
+import subprocess
+import pytest
+import tempfile
+
 from ament_pep257.main import main as pep257_main
 from ament_flake8.main import main_with_errors as flake8_main_with_errors
 from ament_copyright.main import main as copyright_main
-from pathlib import Path
-import pytest
-import tempfile
 
 from rpk import rpk
 rpk.PKG_PATH = (
@@ -25,10 +27,10 @@ rpk.PKG_PATH = (
 )
 
 
-@pytest.mark.parametrize('category, template, robot, path',
+@pytest.mark.parametrize('category, template, robot',
                          [
-                            (category, tpl, robot, tempfile.mkdtemp())
-                            for robot in ['generic', 'ari', 'tiago']
+                            (category, tpl, robot)
+                            for robot in rpk.AVAILABLE_ROBOTS
                             for category, tpls in rpk.TEMPLATES_FAMILIES.items()
                             for tpl in tpls["src"].keys()
                          ])
@@ -36,7 +38,9 @@ rpk.PKG_PATH = (
 @pytest.mark.pep257
 @pytest.mark.flake8
 @pytest.mark.copyright
-def test_generation(category, template, robot, path):
+def test_generation_linting(category, template, robot):
+
+    path = tempfile.mkdtemp()
 
     rpk.main(['create',
               '--robot', robot,
@@ -55,3 +59,45 @@ def test_generation(category, template, robot, path):
 
     rc = copyright_main(argv=[path, 'test'])
     assert rc == 0, 'Found copyright-related errors'
+
+
+@pytest.mark.parametrize('category, template, robot',
+                         [
+                            (category, tpl, robot)
+                            # for robot in rpk.AVAILABLE_ROBOTS
+                            for robot in ['generic']
+                            for category, tpls in rpk.TEMPLATES_FAMILIES.items()
+                            for tpl in tpls["src"].keys()
+                         ])
+def test_generation_compile(category, template, robot):
+
+    ws_dir = Path(tempfile.mkdtemp(suffix='_ws'))
+
+    rpk.main(['create',
+              '--robot', robot,
+              '--path', str(ws_dir / "src"),
+              category,
+              '--template', template,
+              '--id', 'test_node',
+              '--yes'])
+
+    # someone, somewhere, though it would be clever to force python not to write bytecode
+    # when called in a subprocess. This is a problem when using colcon, as it will try to
+    # compile the python files and will fail if it can't write the bytecode.
+    # We need to unset this variable in order to compile
+    completed_process = subprocess.run(" ".join(['PYTHONDONTWRITEBYTECODE=""',
+                                                 'colcon',
+                                                 '--log-level', 'error',
+                                                 'build',
+                                                 '--base-paths', str(ws_dir),
+                                                 '--build-base', str(ws_dir / "build"),
+                                                 '--install-base', str(ws_dir / "install"),
+                                                 ]), shell=True, capture_output=True, check=True)
+
+    # for some reason, the return code is always 0
+    # assert completed_process.returncode == 0
+
+    # ...instead, we need to check that there are no errors in the stderr
+    assert completed_process.stderr == b'', (f"Error in template {category}/{template} "
+                                             f"(robot: {robot}):\n"
+                                             f"{completed_process.stderr.decode('utf-8')}")
